3a513130ceb6b997a3a54bebb8b1b9592e35ae73
Anthony Mariano <33204365+AnthonyMdM@users.noreply.github.com>
2025-08-26 17:56:36 -0300

    Projeto Criado (Project Create)

---
+++ src/main/java/com/ProjectAdmin/AdministraVenda/AdministraVendaApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectAdmin.AdministraVenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministraVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministraVendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectAdmin/AdministraVenda/AdministraVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectAdmin.AdministraVenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministraVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
